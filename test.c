99cd5684cb1859ec2e1430b6f70f4654ec0d9e93
mingeun0507 <tqdlfkg@naver.com>
2019-10-14 10:59:50 +0100

    first commit

--- test.c
+++ test.c
@@ -5,7 +5,7 @@ int main(){
 	system("git status");
 	system("git add --all");
 	system("git commit -m \"first commit\"");
-	system("git push origin master");	
+	system("git push origin master https://github.com/mingeun0507/opensource_project");	
 
 	return 0;
 }
