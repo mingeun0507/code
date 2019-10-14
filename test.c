2fe20c271e65e81b8af311f9c822291cca3126b5
mingeun0507 <tqdlfkg@naver.com>
2019-10-14 11:01:56 +0100

    first commit

--- test.c
+++ test.c
@@ -5,7 +5,7 @@ int main(){
 	system("git status");
 	system("git add --all");
 	system("git commit -m \"first commit\"");
-	system("git push");	
+	system("git push origin master");	
 
 	return 0;
 }
